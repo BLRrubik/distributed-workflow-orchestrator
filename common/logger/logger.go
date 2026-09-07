ce65a2394f7c5cebc319d304276da4349b691446
rubanik.roman <rubanik.roman@wb.ru>
2026-09-07 19:46:54 +0300

    add logger

---
+++ common/logger/logger.go
@@ -0,0 +1,66 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/lmittmann/tint"
+)
+
+type LogLevel byte
+
+const (
+	INFO  LogLevel = 0
+	DEBUG LogLevel = 1
+	WARN  LogLevel = 2
+	ERROR LogLevel = 3
+)
+
+func toSlogLevel(level LogLevel) slog.Level {
+	switch level {
+	case DEBUG:
+		return slog.LevelDebug
+	case WARN:
+		return slog.LevelWarn
+	case ERROR:
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
+}
+
+type Logger struct {
+	*slog.Logger
+}
+
+func New(level LogLevel, isDev bool) *Logger {
+	handler := tint.NewHandler(os.Stdout, &tint.Options{
+		Level: toSlogLevel(level),
+	})
+
+	if !isDev {
+		handler = slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: toSlogLevel(level),
+		})
+	}
+
+	return &Logger{
+		Logger: slog.New(handler),
+	}
+}
+
+func (l *Logger) Info(msg string, args ...any) {
+	l.Logger.Info(msg, args...)
+}
+
+func (l *Logger) Debug(msg string, args ...any) {
+	l.Logger.Debug(msg, args...)
+}
+
+func (l *Logger) Warn(msg string, args ...any) {
+	l.Logger.Warn(msg, args...)
+}
+
+func (l *Logger) Error(msg string, args ...any) {
+	l.Logger.Error(msg, args...)
+}
---
+++ common/logger/wrappers.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"log/slog"
+	"strings"
+)
+
+func Error(err error) slog.Attr {
+	return slog.Any("error", err)
+}
+
+func String(key, msg string) slog.Attr {
+	return slog.String(key, msg)
+}
+
+func Any(key string, data any) slog.Attr {
+	return slog.Any(key, data)
+}
+
+func Int(key string, data int) slog.Attr {
+	return slog.Int(key, data)
+}
+
+func UInt64(key string, data uint64) slog.Attr {
+	return slog.Int(key, int(data))
+}
+
+func Bool(key string, data bool) slog.Attr {
+	return slog.Bool(key, data)
+}
+
+func UInt32(key string, data uint32) slog.Attr {
+	return slog.Int(key, int(data))
+}
+
+func Other(fields ...string) slog.Attr {
+	var str strings.Builder
+	if len(fields)%2 != 0 {
+		return slog.String("other", strings.Join(fields, ", "))
+	}
+
+	str.Grow(20 * len(fields))
+	for i := 0; i+1 < len(fields); i += 2 {
+		str.WriteString(fields[i])
+		str.WriteString(": ")
+		str.WriteString(fields[i+1])
+		if i+2 < len(fields) {
+			str.WriteString(", ")
+		}
+	}
+
+	return slog.String("other", str.String())
+}
